c20a00e2abc5199a9fee1fa65103736642c75086
skfaysal <faysal@smart-retina.com>
2021-03-11 00:14:16 +0600

    Class based api define to add two numbers

--- diabApi/views.py
+++ diabApi/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 import pickle
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import render
 import numpy as np
 
-# Create your views here.
+# Function based view to add numbers
 @api_view(['GET', 'POST']) # A Decorator to define an API function in Django. It's converts  python function into API function
 def api_add(request):
     sum = 0
@@ -23,6 +24,18 @@ def api_add(request):
             sum += data[key]
         response_dict = {"sum": sum}
     return Response(response_dict, status=status.HTTP_201_CREATED)
+
+
+# Class based view to add numbers
+class Add_Values(APIView):
+    def post(self, request, format=None):
+        sum = 0
+        # Add the numbers
+        data = request.data
+        for key in data:
+            sum += data[key]
+        response_dict = {"sum": sum}
+        return Response(response_dict, status=status.HTTP_201_CREATED)
 
 
 """
